f38c39c4cc62d36207e05d0225aaaa936f25de20
Oscar Moll <orm@csail.mit.edu>
2023-09-07 13:37:42 -0400

    tar based sync

--- sync_tools.bash
+++ sync_tools.bash
@@ -1,73 +1,122 @@
-PATH=$HOME/fpart_install/bin:$PATH
-MAMBA_GLOBAL=/home/gridsan/omoll/mambaforge_backup/
-MAMBA_LOCAL=/state/partition1/user/omoll/mambaforge/
+
+## rationale: local filesystem is fast and allows locking (used by conda and other programs), 
+## global is slow for metadata but ok for steamed reading large files.
+## then, tar from local into global, untar from global into local
+## this works much better than parallel rsyncs, which takes forever to start bc the analysis phase is
+## quite slow for directories with many files on the global filesystem, so even if there are no updates
+## to make, rsync takes a long time to check.
+
+function sync_tar_to_dir() {
+    TAR=$1
+    DIR=$2
+
+    if [[ -z $TAR || -z $DIR ]]; then
+        echo 'must specify tar and dir'
+        return 1
+    fi
+
+    if [[ ! -e $TAR ]]; then
+        echo 'tar '$TAR' does not exist'
+        return 1
+    fi
+
+    TEMP=`mktemp -d`
+    GLOBAL_TIME=`stat -c '%y' $TAR`
+
+    if [ -e  $DIR/.sync.time ]; then
+        LOCAL_TIME=`stat -c '%y' $DIR/.sync.time`
+    else
+        LOCAL_TIME='0'
+    fi
+
+    # check if local time < global time, if not, return 0
+    if [[ $LOCAL_TIME > $GLOBAL_TIME ]]; then
+        #echo 'local time '$LOCAL_TIME' > global time '$GLOBAL_TIME', skipping sync'
+        return 0
+    fi
+
+    echo 'syncing '$TAR 'to' $DIR
+    tar -xf $TAR -C $TEMP && \
+        rm -rf $DIR && \
+        mv $TEMP $DIR && \
+        touch $DIR/.sync.time
+    if [ $? -eq 0 ]; then
+        echo 'done syncing '$TAR 'to' $DIR
+        return 0
+    else
+        echo 'warning: problem syncing '$TAR 'to' $DIR
+        return 1
+    fi
+}
+
+
+MAMBA_GLOBAL=/home/gridsan/omoll/mambaforge_tars/
+MAMBA_LOCAL=/state/partition1/user/$USER/mambaforge/
+
 
 function sync_conda_global_to_worker() {
     ENV=$1
 
-    ## avoid expensive rsync filesystem checks in the common case, by using the license file as a signal.
-    LOCAL_TIME=`stat -c '%y' $MAMBA_LOCAL/LICENSE.txt`
-    GLOBAL_TIME=`stat -c '%y' $MAMBA_GLOBAL/LICENSE.txt`
- 
     # check we are running in host with name of form d-*, otherwise error out
     if [[ `hostname` != d-* ]]; then 
     	echo 'must run in work node, not login'    
         return 1
     fi
-        
-    # check if env exists
-    if [[ ! -d $MAMBA_GLOBAL/envs/$ENV ]]; then
-    	echo 'env '$ENV' does not exist in global'
-        return 1
+
+    sync_tar_to_dir $MAMBA_GLOBAL/base.tar $MAMBA_LOCAL || return 1
+
+    if [ ! -z $ENV ]; then
+        sync_tar_to_dir $MAMBA_GLOBAL/envs/$ENV.tar $MAMBA_LOCAL/envs/$ENV
+        if [ $? -eq 0 ]; then   
+            echo 'done syncing '$ENV
+        else
+            echo 'warning: problem syncing '$ENV
+            return 1
+        fi
     fi
-
-    if [[ $LOCAL_TIME != $GLOBAL_TIME ]]; then
-    	echo "syncing mamba $ENV from global to local"
-        # rsync $OPTIONS $MAMBA_GLOBAL $MAMBA_LOCAL
-        mkdir -p $MAMBA_LOCAL
-        mkdir -p $MAMBA_LOCAL/envs/$ENV
-
-    	# rsync -av --filter="- /pkgs/**" --filter="- /include/**" --filter="- __pycache__" --filter="- /envs/**" $MAMBA_GLOBAL $MAMBA_LOCAL
-        fpsync -n 80 -v -o '-a --exclude="/pkgs/**" --exclude="/include/**" --exclude="__pycache__" --exclude="/envs/**"' $MAMBA_GLOBAL $MAMBA_LOCAL
-        fpsync -n 80 -v -o '-a --exclude="/include/**" --exclude="/*/include/**"' $MAMBA_GLOBAL/envs/$ENV/ $MAMBA_LOCAL/envs/$ENV/
-
-    	EXIT=$?
-	    [ $EXIT -eq 0 ] && echo 'done syncing conda env' || echo 'Warning: problem syncing conda'
-    else
-    	echo 'LICENSE.txt up to date, skipping fpsync. Touch LICENSE.txt to force update'
-	    EXIT=0
-    fi
-    
-    return $EXIT
 }
 
 function sync_conda_login_to_global() {
     echo 'backing up login env to global'
+    ENV=$1
     if [[ `hostname` != login-4 ]]; then # only do from login-4, otherwise --delete flag would erase
     	echo 'must run in login-4'
 	    return 1
     fi
 
-    mamba clean -a # clean up old pkgs to avoid long copies
-
-    fpsync -n 180 -v -o '-av --delete --exclude="__pycache__/**"' $MAMBA_LOCAL $MAMBA_GLOBAL
-    return $EXIT
-}
-
-function restore_conda_global_to_login() {
-    # only used when login-4 is cleared
-    if [[ `hostname` != login-4 ]]; then # only do from login-4, otherwise could erase
-    	echo 'run in login-4'
-	    return 1
+    # mambalocal must exist
+    if [[ ! -d $MAMBA_LOCAL ]]; then
+        echo 'mamba local does not exist'
+        return 1
     fi
 
-    fpsync -n 180 -v -o '-av --exclude="__pycache__/**"' $MAMBA_GLOBAL $MAMBA_LOCAL
+    mkdir -p $MAMBA_GLOBAL/envs
+
+    echo 'backing up base env...'
+    tar -cf $MAMBA_GLOBAL/base.tar -C $MAMBA_LOCAL/ --exclude=envs/* --exclude=pkgs/* .
+    echo 'done backing up base'
+
+    if [[ -z $ENV ]]; then
+        echo 'syncing all envs'
+        for env in `ls $MAMBA_LOCAL/envs/`; do
+            echo 'syncing env '$env
+            tar -cf $MAMBA_GLOBAL/envs/$env.tar -C $MAMBA_LOCAL/envs/$env/ .
+            echo 'synced env '$env to $MAMBA_GLOBAL/envs/$env.tar
+        done
+    else
+        if [[ ! -d $MAMBA_LOCAL/envs/$ENV ]]; then
+            echo 'env '$ENV' does not exist in local'
+            return 1
+        fi
+        tar -cf $MAMBA_GLOBAL/envs/$ENV.tar -C $MAMBA_LOCAL/envs/$ENV/ .
+        echo 'synced env '$ENV to $MAMBA_GLOBAL/envs/$ENV.tar
+    fi
 }
+
 
 function setup_worker_mamba() {
     ENV=$1
     sync_conda_global_to_worker $ENV
     . $MAMBA_LOCAL/etc/profile.d/conda.sh
     . $MAMBA_LOCAL/etc/profile.d/mamba.sh
-    mamba activate $ENV
 }
