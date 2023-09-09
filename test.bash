ebbc9de291e6bedf9c5d15f3255c1b20de0a2733
Oscar Moll <orm@csail.mit.edu>
2023-09-09 12:31:46 -0400

    omp

--- test.bash
+++ test.bash
@@ -8,4 +8,9 @@ fi
 
 export PYTHONNOUSERSITE=1
 mamba activate open_flamingo
+
+if [ ! -z $SLURM_CPUS_PER_TASK ]; then
+    export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+fi
+
 python /home/gridsan/omoll/supercloud_util/test.py
